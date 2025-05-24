8c5ff9615783f041c6e76bf46260156d78ac424d
Rafael Augusto Gelain <rafael.augustogelain2@gmail.com>
2025-05-24 13:55:08 -0300

    first commit to git

---
+++ src/main/java/com/despesasPessoal/DespesasPessoal/DespesasPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.despesasPessoal.DespesasPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasPessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/despesasPessoal/DespesasPessoal/DespesasPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despesasPessoal.DespesasPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesasPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
